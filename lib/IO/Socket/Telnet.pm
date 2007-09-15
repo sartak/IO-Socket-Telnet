5d581467c4f69399ec1eb371f96511bd4b30ead6
Shawn M Moore <sartak@gmail.com>
2007-09-15 00:12:42 -0500

    Most of the implementation. almost all good except for callbacks

--- lib/IO/Socket/Telnet.pm
+++ lib/IO/Socket/Telnet.pm
@@ -5,7 +5,120 @@ use warnings;
 use parent 'IO::Socket::INET';
 use Class::Method::Modifiers;
 
+around new => sub
+{
+    my $orig = shift;
+    my $self = $orig->(@_);
 
+    ${*$self}{telnet_mode} = 'normal';
+    ${*$self}{telnet_sb_buffer} = '';
+
+    return $self;
+};
+
+after recv => sub
+{
+    $_[1] = $_[0]->_parse($_[1]);
+};
+
+my $IAC = chr(255);
+my $SB = chr(250);
+my $SE = chr(240);
+
+my $WILL = chr(251);
+my $WONT = chr(252);
+my $DO = chr(253);
+my $DONT = chr(254);
+
+my %dispatch =
+(
+    normal => sub
+    {
+        my ($self, $c) = @_;
+        return $c unless $c eq $IAC;
+        return (undef, 'iac');
+    },
+
+    iac => sub
+    {
+        my ($self, $c) = @_;
+        return ($IAC, 'normal') if $c eq $IAC;
+        return (undef, 'do')    if $c eq $DO;
+        return (undef, 'dont')  if $c eq $DONT;
+        return (undef, 'will')  if $c eq $WILL;
+        return (undef, 'wont')  if $c eq $WONT;
+        return (undef, 'sb')    if $c eq $SB;
+    },
+
+    do => sub
+    {
+        my ($self, $c, $m) = @_;
+        $self->_telnet_simple_callback($m, $c);
+        return (undef, 'normal');
+    },
+
+    sb => sub
+    {
+        my ($self, $c) = @_;
+        return (undef, 'sbiac') if $c eq $IAC;
+        return (undef, undef, $c);
+    },
+
+    sbiac => sub
+    {
+        my ($self, $c) = @_;
+        if ($c eq $IAC)
+        {
+            ${*$self}{telnet_sb_buffer} .= $IAC;
+            return (undef, 'sb');
+        }
+
+        if ($c eq $SE)
+        {
+            $self->_telnet_complex_callback();
+            ${*$self}{telnet_sb_buffer} = '';
+            return (undef, 'normal');
+        }
+
+        # IAC followed by something other than IAC and SE.. what??
+        require Carp;
+        Carp::croak "Invalid telnet stream: IAC SE ... IAC $c";
+    },
+);
+
+$dispatch{dont} = $dispatch{will} = $dispatch{wont} = $dispatch{do};
+
+sub _parse
+{
+    my ($self, $in) = @_;
+    my $out = '';
+
+    C: for my $c (split '', $in)
+    {
+        my ($o, $m)
+            = $dispatch{${*$self}{telnet_mode}}
+                ->($self, $c, ${*$self}{telnet_mode});
+
+        defined $o and $out .= $o;
+        defined $m and ${*$self}{telnet_mode} = $m;
+    }
+
+    return $out;
+}
+
+sub _telnet_simple_callback
+{
+    my ($self, $char, $mode) = @_;
+    ${*$self}{telnet_simple_cb} or return;
+    ${*$self}{telnet_simple_cb}->($self, $char, $mode);
+}
+
+sub _telnet_complex_callback
+{
+    my ($self, $sb) = @_;
+    ${*$self}{telnet_complex_cb} or return;
+    ${*$self}{telnet_complex_cb}->($self, $sb);
+}
 
 =head1 NAME
 
