e30260c3846a0cf5977cb1685c8c7950c9946f07
Shawn M Moore <sartak@gmail.com>
2007-09-14 23:05:55 -0500

    Initial import of IO::Socket::Telnet

---
+++ lib/IO/Socket/Telnet.pm
@@ -0,0 +1,83 @@
+#!perl
+package IO::Socket::Telnet;
+use strict;
+use warnings;
+
+
+
+=head1 NAME
+
+IO::Socket::Telnet - ???
+
+=head1 VERSION
+
+Version 0.01 released ???
+
+=cut
+
+our $VERSION = '0.01';
+
+=head1 SYNOPSIS
+
+    use IO::Socket::Telnet;
+    do_stuff();
+
+=head1 DESCRIPTION
+
+
+
+=head1 SEE ALSO
+
+L<Foo::Bar>
+
+=head1 AUTHOR
+
+Shawn M Moore, C<< <sartak at gmail.com> >>
+
+=head1 BUGS
+
+No known bugs.
+
+Please report any bugs through RT: email
+C<bug-io-socket-telnet at rt.cpan.org>, or browse to
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=IO-Socket-Telnet>.
+
+=head1 SUPPORT
+
+You can find this documentation for this module with the perldoc command.
+
+    perldoc IO::Socket::Telnet
+
+You can also look for information at:
+
+=over 4
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/IO-Socket-Telnet>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/IO-Socket-Telnet>
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=IO-Socket-Telnet>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/IO-Socket-Telnet>
+
+=back
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2007 Shawn M Moore.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
+
+1;
+
