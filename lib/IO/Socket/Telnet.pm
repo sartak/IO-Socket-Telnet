2989d5132a23a55ef84ec61c38bbd973dbcfda9f
Shawn M Moore <sartak@gmail.com>
2009-06-04 04:40:07 -0400

    POD fixes

--- lib/IO/Socket/Telnet.pm
+++ lib/IO/Socket/Telnet.pm
@@ -4,6 +4,8 @@ use warnings;
 use base 'IO::Socket::INET';
 use Class::Method::Modifiers;
 
+our $VERSION = '0.03';
+
 around new => sub {
     my $orig = shift;
     my $class = shift;
@@ -270,17 +272,11 @@ sub _telnet_complex_callback {
     ${*$self}{telnet_complex_cb}->($self, $sb);
 }
 
+__END__
+
 =head1 NAME
 
 IO::Socket::Telnet - transparent telnet negotiation for IO::Socket::INET
-
-=head1 VERSION
-
-Version 0.03 released ???
-
-=cut
-
-our $VERSION = '0.03';
 
 =head1 SYNOPSIS
 
@@ -417,50 +413,12 @@ L<Net::Telnet>, L<IO::Socket::INET>, L<IO::Socket>, L<IO::Handle>
 
 Shawn M Moore, C<< <sartak at gmail.com> >>
 
-=head1 BUGS
-
-No known bugs.
-
-Please report any bugs through RT: email
-C<bug-io-socket-telnet at rt.cpan.org>, or browse to
-L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=IO-Socket-Telnet>.
-
-=head1 SUPPORT
-
-You can find this documentation for this module with the perldoc command.
-
-    perldoc IO::Socket::Telnet
-
-You can also look for information at:
-
-=over 4
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/IO-Socket-Telnet>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/IO-Socket-Telnet>
-
-=item * RT: CPAN's request tracker
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=IO-Socket-Telnet>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/IO-Socket-Telnet>
-
-=back
-
 =head1 COPYRIGHT AND LICENSE
 
-Copyright 2007 Shawn M Moore.
+Copyright 2007, 2009 Shawn M Moore.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
 
 =cut
-
-1;
 
